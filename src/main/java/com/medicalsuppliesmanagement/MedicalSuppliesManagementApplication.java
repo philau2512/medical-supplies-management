0873d7336d334649e62803b8b82ab2fc0a171532
philau2512 <dplau25122002@gmail.com>
2025-07-25 19:21:23 +0700

    first commit

---
+++ src/main/java/com/medicalsuppliesmanagement/MedicalSuppliesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.medicalsuppliesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalSuppliesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalSuppliesManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medicalsuppliesmanagement/MedicalSuppliesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalsuppliesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalSuppliesManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
